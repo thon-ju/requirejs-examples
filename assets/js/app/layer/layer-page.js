7310a94d3bf8202703a803a0dc068a0bea371f78
thon <thon@gmail.com>
2014-11-22 14:16:13 +0800

    add plugins, update config

--- assets/js/app/layer/layer-page.js
+++ assets/js/app/layer/layer-page.js
@@ -6,8 +6,44 @@ require(['jquery','jquery.layer'], function () {
     $('#click1').click(function() {
         layer.alert('Just a alert', 6); //风格一
     });
-    $('#click2').click(function(){
-        layer.msg('Just a tip',5,1); //风格二
+    $('#msg1').click(function(){
+        layer.msg('Just a tip',1,1); //风格二
+    });
+    $('#msg2').click(function(){
+        layer.msg('Just a tip',1,2); //风格二
+    });
+    $('#msg3').click(function(){
+        layer.msg('Just a tip',1,3); //风格二
+    });
+    $('#msg4').click(function(){
+        layer.msg('Just a tip',1,4); //风格二
+    });
+    $('#msg5').click(function(){
+        layer.msg('Just a tip',1,5); //风格二
+    });
+    $('#msg6').click(function(){
+        layer.msg('Just a tip',1,6); //风格二
+    });
+    $('#msg7').click(function(){
+        layer.msg('Just a tip',1,7); //风格二
+    });
+    $('#msg8').click(function(){
+        layer.msg('Just a tip',1,8); //风格二
+    });
+    $('#msg9').click(function(){
+        layer.msg('Just a tip',0,9); //风格二
+    });
+    $('#msg10').click(function(){
+        layer.msg('Just a tip',1,10); //风格二
+    });
+    $('#msg11').click(function(){
+        layer.msg('Just a tip',1,11); //风格二
+    });
+    $('#msg12').click(function(){
+        layer.msg('Just a tip',1,12); //风格二
+    });
+    $('#msg13').click(function(){
+        layer.msg('Just a tip',1,13); //风格二
     });
     $('#loading1-click').click(function(){
         layer.load('Loading...', 0);
